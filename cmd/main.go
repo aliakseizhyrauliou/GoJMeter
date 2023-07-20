bcac5b96d913642853e95f68a5922f40c90c2733
AlexeyKidA <zhuraulioualiaksei@gmail.com>
2023-07-20 12:43:56 +0300

    add postman

--- cmd/main.go
+++ cmd/main.go
@@ -1,25 +1,22 @@
 package main
 
-
 import (
+	"JMeter_Go/internal/config"
+	"JMeter_Go/internal/handlers"
 	"fmt"
 	"net/http"
-	"JMeter_Go/internal/handlers"
-	"JMeter_Go/internal/config"
 )
-
 
 func main() {
 
 	cfg := config.LoadConfig()
 	port := cfg.Port
 
-    fmt.Println("App running on Port", port)
+	fmt.Println("App running on Port", port)
 
 	http.HandleFunc("/hello", handlers.HelloHandler)
 	http.HandleFunc("/upload-jmx", handlers.UploadFileHandler)
 	http.HandleFunc("/run-tests", handlers.TestRunnerHandler)
 
-	
 	http.ListenAndServe(port, nil)
 }
